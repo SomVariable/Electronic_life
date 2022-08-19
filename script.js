cdfab19be55e1114af9e23dd0150732cf34c9e87
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-08-19 14:40:43 +0300

    1) Added a creature action ranged

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const plan = ["###################################",
-              "# ****************                #",
-              "#      ####                *****  #",
-              "# #### o      ####                #",
-              "#      ####        **********     #",
-              "#####o                            #",
-              "#     ***    ***************      #",
-              "#                                 #",
-              "#  *********************   o      #",
-              "#         o                       #",
-              "#      ********************       #",
-              "###################################",]
+const plan = ["############################################################################################################################################################",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "# o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#      o #   *#     #",
+              "#   ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*       ######*    #",
+              "#   *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *       *     *    #",
+              "############################################################################################################################################################"]
 
 const directions = {
     "n" : new Vector( 0, -1),
@@ -32,15 +64,15 @@ const growAC = () => {
     return {type: "grow"}
 }
 const reproduceAC = (payload) => {
-    return {type: "reproduce", direction: payload} 
+    return {type: "reproduce", direction: payload.dir, dist: payload.dist} 
 }
 
 const moveAC = (payload) => {
-    return {type: "move", direction: payload} 
+    return {type: "move", direction: payload.dir, dist: payload.dist} 
 }
 
 const eatAC = (payload) => {
-    return {type: "eat", direction: payload} 
+    return {type: "eat", direction: payload.dir, dist: payload.dist} 
 }
 //end.ActionCreaters------------------------------------------------------
 
@@ -59,7 +91,7 @@ actionTypes.move = function(critter, vector, action) {
     
     if(isIncorrectDest || isIncorrectEnergy || this.grid.get(dest) !== null) return false
 
-    critter.changeEnergy(-1)
+    this.deleteCreature(critter, vector, -1)
     this.grid.set(vector, null);
     this.grid.set(dest, critter)
     return true
@@ -83,7 +115,7 @@ actionTypes.reproduce = function(critter, vector, action)  {
           isIncorrectEnergy =  critter.energy <= 2 * baby.energy;
 
     if(isIncorrectDest || isIncorrectEnergy || this.grid.get(dest) !== null) return false
-    critter.changeEnergy(-2 * baby.energy);
+    this.deleteCreature(critter, vector, -2 * baby.energy)
     this.grid.set(dest, baby)
     return true
 }
@@ -120,8 +152,18 @@ function Vector(x, y){
 }
 
 Vector.prototype.plus = function(other) {
+    if(!other || other.__proto__ !== Vector.prototype) {
+        return new Vector(this.x, this.y)
+    }
     return new Vector(this.x + other.x, this.y + other.y)
-} 
+}
+
+Vector.prototype.subtraction = function(other) {
+    if(!other || other.__proto__ !== Vector.prototype) {
+        return new Vector(this.x, this.y)
+    }
+    return new Vector(this.x - other.x, this.y - other.y)
+}
 
 //Grid--------------------------------------------------------------------------
 
@@ -132,14 +174,19 @@ function Grid(width, height){
 }
 
 Grid.prototype.isInside = function (vector){
-    return vector.x >= 0 && vector.x < this.width &&
-           vector.y >= 0 && vector.y < this.height
+    const isInsideByX = vector.x >= 0 && vector.x < this.width,
+          isIsideByY  = vector.y >= 0 && vector.y < this.height
+    return isInsideByX && isIsideByY;
+           
 }
 
 Grid.prototype.set = function (vector, value) {
     this.space[vector.x + this.width * vector.y] = value
 }
 Grid.prototype.get = function (vector) {
+    if(!vector){
+        const fff = null;
+    }
     return this.space[vector.x + this.width * vector.y]
 }
 
@@ -170,6 +217,7 @@ function Creature(){
 
 Creature.prototype.changeEnergy = function (val) {
     this.energy += val;
+    
 }
 
 //Plant--------------------------------------------------------------
@@ -181,8 +229,8 @@ Plant.prototype = Object.create(Creature.prototype)
 
 Plant.prototype.act = function(context){
     if(this.energy > 15){
-        const space = context.find(" ");
-        if(space) return reproduceAC(space)
+        const space = context.find(" ", 1);
+        if(space) return reproduceAC({dist: space.dist, dir: space.dir})
     }
 
     if(this.energy < 20){
@@ -191,46 +239,53 @@ Plant.prototype.act = function(context){
 }
 //end. Plant---------------------------------------------------------
 
-//PlantEater--------------------------------------------------------------
+//PlantEater---------------------------------------------------------
 function PlantEater(){
     this.energy = 20;
+    this.range = 1
 }
 
 PlantEater.prototype = Object.create(Creature.prototype)
 
 PlantEater.prototype.act = function(context){
-    const space = context.find(" ");
-    if(this.energy > 60 && space) return reproduceAC(space)
+    const space = context.find(" ", 1);
+    if(this.energy > 60 && space) return reproduceAC({dist: space.dist, dir: space.dir})
     
-    const plant= context.find(creatureViews.plant)
+    const plant= context.find(creatureViews.plant, this.range)
 
-    if(plant) return eatAC(plant)
+    if(plant) return eatAC({plant})
 
-    if(space) return moveAC(space)
+    if(space) return moveAC({dist: space.dist, dir: space.dir})
 }
-//end. PlantEater---------------------------------------------------------
+//end. PlantEater------------------------------------------------------
 
-//BouncingCritter-----------------------------------------------------
+//PlantEater---------------------------------------------------------
+function SmartPlantEater(){
+    this.energy = 20;
+    this.range = 3;
+}
 
+SmartPlantEater.prototype = Object.create(Creature.prototype)
 
-// function BouncingCritter (){
-//     this.direction = randomElement(Object.keys(directions))
-// }
+SmartPlantEater.prototype.act = function(context){
+    const space = context.find(" ", 1);
+    if(this.energy > 60 && space) return reproduceAC({dist: space.dist, dir: space.dir})
+    
+    const plant= context.find(creatureViews.plant, this.range)
 
+    if(plant && (plant.dist.x !== directions[plant.dir].x || plant.dist.y !== directions[plant.dir].y)){
+        return moveAC({dist: plant.dist, dir: plant.dir})
+    }
+    if(plant) return eatAC({dir: plant.dir, dist: plant.dist})
 
-// BouncingCritter.prototype.act = function (view) {
-//     if(view.look(this.direction) !== " ") this.direction = view.find(" ") || "s";
-//     return {type: "move", direction: this.direction}
-// }
-
-//end. BouncingCritter-------------------------------------------------
+    if(space) return moveAC({dist: space.dist, dir: space.dir})
+}
+//end. PlantEater------------------------------------------------------
 
 //WallFollower---------------------------------------------------------
 
-
-
 // function WallFollower (){
-//     this.direction = "s"
+//     this.energy = 20;
 // }
 
 // WallFollower.prototype.act = function (view) {
@@ -245,7 +300,7 @@ PlantEater.prototype.act = function(context){
 //         if(this.direction === start) break
 //     }
 
-//     return {type: "move", direction: this.direction}
+//     return moveAC(this.direction)
 // }
 
 //end. WallFollower-----------------------------------------------------
@@ -298,7 +353,14 @@ World.prototype.letAct = function(critter, vector){
     const handled = isCorrectAction && actionTypes[action.type].call(this, critter, vector, action);
 
     if(!handled){
-        critter.changeEnergy(-0.2);
+        this.deleteCreature(critter, vector, -0.2)
+    }
+}
+
+World.prototype.deleteCreature = function(critter, vector, subtrahend){
+    critter.changeEnergy(subtrahend);
+    if(critter.energy <= 1){
+        this.grid.set(vector, null)
     }
 }
 
@@ -314,31 +376,86 @@ function View(world, vector){
     this.vector = vector;
 }
 
-View.prototype.look = function(dir){
-    const target = this.vector.plus(directions[dir])
-    return this.world.grid.isInside(target) ? charFromElement(this.world.grid.get(target)) : "#" 
+View.prototype.look = function(range){
+    const isInside = this.world.grid.isInside(range);
+
+    if(isInside) {
+        const char = charFromElement(this.world.grid.get(range))
+        return char;
+    } else{
+        return '#'
+    }
+    //return  ? ) : "#" 
 }
 
-View.prototype.findAll = function(ch){
-    const found = [];
+//thre are recursion where we look in max-range distance if there is no el then we look on range -1 e.t
+View.prototype.lookArraund = function(ch, dir, range){
+    const direction = directions[dir]
+    const char = this.look(this.vector.plus(range));
+    if(char === ch) return range
+    if(range.x === direction.x && range.y === direction.y) return false
+    return this.lookArraund(ch, dir, range.subtraction(new Vector(direction.x, direction.y)))
+}
+
+View.prototype.findAll = function(ch, range){
+    let found = null;
     
+
     for(let dir in directions){
-        if(this.look(dir) === ch) found.push(dir)
+        const _range = new Vector(directions[dir].x * range, directions[dir].y * range)
+        const _found = this.lookArraund(ch, dir, _range)
+        if(_found) {
+            found = {dir: dir, dist: _found};
+            break;
+        }
     }
 
-    return found
+    if(found){
+        return found
+    }else{
+        return null
+    }
 }
 
-View.prototype.find = function(ch){
-    const found = this.findAll(ch);
-    return found.length === 0 ? null : randomElement(found)
+View.prototype.find = function(ch, range){
+    const found = this.findAll(ch, range);
+    return found? found : null
+}
+
+View.prototype.findFood = function(ch, range){
+    const found = this.findAll(ch, range);
+    return found? null : randomElement(found)
 }
 
 function Wall(){}
 
+//const world = new World(plan, {"#": Wall, "*": Plant, "o": PlantEater});
 
+const planConstructor = (x, y) => {
+    const _plan = []
 
-const world = new World(plan, {"#": Wall, "*": Plant, "o": PlantEater});
+    for(let i = 0; i < y; i++){
+        let str = ''
+        _plan.push()
+        
+        for(let j = 0; j < x; j++){
+            if(i === 0) _plan[i].push('#')
+            else if(i+1 === y) _plan[i].push('#')
+            else if(j === 0 || j + 1 === x){
+                _plan[i].push('#')
+            }else{
+                _plan[i].push(' ')
+            }
+
+            
+        }
+    }
+
+    return _plan
+}
+// const newPlan = planConstructor(20, 20)
+// console.log(newPlan)
+const world = new World(plan, {"#": Wall, "*": Plant, "o": SmartPlantEater});
 
 
 const direction = setInterval(() => {
